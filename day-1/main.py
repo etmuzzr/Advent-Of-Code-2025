126ec18ebcbadfa956b9de8b9fe4dbe0e2f6127d
ethan <ethanmurray180@gmail.com>
2025-12-02 00:58:23 +0000

    added gitignore and removed unnecessary comments from day-1

--- day-1/main.py
+++ day-1/main.py
@@ -2,21 +2,21 @@
 # PART 1
 #===========
 
-# with open('input.txt') as f:
-#     rotations = f.readlines()
-#     rotations = [rot.strip('\n') for rot in rotations]
-#
-# zero_counts = 0
-# dial = 50
-#
-# for rot in rotations:
-#     dir = rot[0]
-#     num_turns = int(rot[1:]) * (1 if dir == 'R' else -1)
-#
-#     dial = (100 + num_turns + dial) % 100
-#
-#     if dial == 0:
-#         zero_counts += 1
+with open('input.txt') as f:
+    rotations = f.readlines()
+    rotations = [rot.strip('\n') for rot in rotations]
+
+zero_counts = 0
+dial = 50
+
+for rot in rotations:
+    dir = rot[0]
+    num_turns = int(rot[1:]) * (1 if dir == 'R' else -1)
+
+    dial = (100 + num_turns + dial) % 100
+
+    if dial == 0:
+        zero_counts += 1
 
 #===========
 # PART 2
