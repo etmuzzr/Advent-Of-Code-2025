8324bbde73d970a9433644be5336668918417b0a
ethan <ethanmurray180@gmail.com>
2025-12-02 17:10:09 +0000

    day 2 part 2

--- day-2/main.py
+++ day-2/main.py
@@ -1,6 +1,7 @@
 #===========
 # PART 1
 #===========
+
 with open('input.txt') as ids:
     ids = ids.read().split(',')
     ids = [(int(first_id), int(last_id)) for first_id, last_id in [id.split('-') for id in ids]]
@@ -15,3 +16,36 @@ for first_id, last_id in ids:
             invalid_ids_sum += int(s)
 
 print(invalid_ids_sum)
+
+#===========
+# PART 2
+#===========
+import re
+
+with open('input.txt') as ids:
+    ids = ids.read().split(',')
+    ids = [(int(first_id), int(last_id)) for first_id, last_id in [id.split('-') for id in ids]]
+
+# could be more efficient but lazy (come on it's crimbo)
+def get_divisors(n):
+    out = []
+    for i in range(1, n):
+        if n % i == 0:
+            out.append(i)
+
+    return out
+
+invalid_ids= set()
+
+for first_id, last_id in ids:
+    for id in range(first_id, last_id + 1):
+        s = str(id)
+        for div in get_divisors(len(s)):
+            bools = []
+            for i in range(0, len(s), div):
+                bools.append(s[i:i+div] == s[:div])
+
+            if all(bools):
+                invalid_ids.add(id)
+
+print(sum(invalid_ids))
