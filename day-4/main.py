e8b551668abc072975a43df856adf0c15267bbb4
ethan <ethanmurray180@gmail.com>
2025-12-05 21:13:29 +0000

    day 4 part 1

---
+++ day-4/main.py
@@ -0,0 +1,39 @@
+#===========
+# PART 1
+#===========
+
+with open('input.txt') as grid:
+    grid = [row[:-1] for row in grid.readlines()]
+
+dirs = [(1,0), (-1,0), (0,1), (0,-1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+n = len(grid)
+m = len(grid[0])
+res = 0
+
+for r in range(n):
+    for c in range(m):
+        if grid[r][c] == '.':
+            continue
+
+        rolls = 0
+        for dir in dirs:
+            dr, dc = dir
+
+            if (r + dr) < 0 or (r + dr) >= n:
+                continue
+
+            if (c + dc) < 0 or (c + dc) >= m:
+                continue
+
+            if grid[r + dr][c + dc] == '@':
+                rolls += 1
+
+        if rolls < 4:
+            res += 1
+
+print(res)
+
+#===========
+# PART 2
+#===========
+
