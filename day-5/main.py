a95a9df146c0157214c96aeddaed980ffbc92ad4
ethan <ethanmurray180@gmail.com>
2025-12-06 00:41:39 +0000

    day 5 part 2

--- day-5/main.py
+++ day-5/main.py
@@ -2,6 +2,8 @@
 # PART 1
 #===========
 
+# I realise a bsearch would be a very good solution to this problem after writing this monstrosity
+
 with open('input.txt') as file:
     fresh_ids = []
     ids = []
@@ -36,3 +38,30 @@ print(res)
 #===========
 # PART 2
 #===========
+
+with open('input.txt') as file:
+    fresh_ids = []
+
+    for id in file.readlines():
+        s = id[:-1]
+        if s == '':
+            break
+
+        start, end = s.split('-')
+        fresh_ids.append((int(start), int(end)))
+
+    fresh_ids.sort()
+
+res = 0
+last_start, last_end = fresh_ids[0]
+
+for start, end in (fresh_ids[1:]):
+    if start > last_end:
+        res += (last_end - last_start) + 1
+        last_start, last_end = start, end
+    else:
+        last_end = max(last_end, end)
+
+res += (last_end - last_start) + 1
+
+print(res)
